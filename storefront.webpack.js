2b10854018feaff4ca2ce33c58f00071147315c0
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2024-02-20 11:20:45 -0300

    chore: set option to get paid shippment paid option

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -6,6 +6,9 @@ module.exports = () => ({
   resolve: {
     alias: {
       //'./html/ProductVariations.html': path.resolve(__dirname, 'template/js/custom-js/html/ProductVariations.html')
+      './js/ShippingCalculator.js': path.resolve(__dirname, 'template/js/custom-js/js/ShippingCalculator.js'),
+      './html/ShippingCalculator.html': path.resolve(__dirname, 'template/js/custom-js/html/ShippingCalculator.html'),
+      
     }
   }
 })
