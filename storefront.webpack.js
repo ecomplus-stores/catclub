e620afaac831d2e2b18bb08dfa234127272fdd63
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2024-02-20 11:45:33 -0300

    chore: replace paths alias

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -5,9 +5,8 @@ const path = require('path')
 module.exports = () => ({
   resolve: {
     alias: {
-      //'./html/ProductVariations.html': path.resolve(__dirname, 'template/js/custom-js/html/ProductVariations.html')
-      './js/ShippingCalculator.js': path.resolve(__dirname, 'template/js/custom-js/js/ShippingCalculator.js'),
-      './html/ShippingCalculator.html': path.resolve(__dirname, 'template/js/custom-js/html/ShippingCalculator.html'),
+      './js/ShippingCalculator.js': path.resolve(__dirname, 'template/js/js/ShippingCalculator.js'),
+      './html/ShippingCalculator.html': path.resolve(__dirname, 'template/js/html/ShippingCalculator.html')
       
     }
   }
